8b00558c97a84e608dbf5f018a6a4d5b63775435
trickaugusto <trickaugusto1206@gmail.com>
2024-06-20 09:12:25 +0000

    first commit: modulos events, prisma e spots, algumas models do banco e config do dev container

---
+++ src/events/events.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class EventsService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createEventDto: CreateEventDto) {
+    return this.prismaService.event.create({
+      data: {
+        ...createEventDto,
+        date: new Date(createEventDto.date),
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.event.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.event.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prismaService.event.update({
+      where: { id },
+      data: {
+        ...updateEventDto,
+        date: new Date(updateEventDto.date),
+      },
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.event.delete({
+      where: { id },
+    });
+  }
+}
